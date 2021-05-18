809ac35e31e05cf9075b276f750af2aaa98f6b5c
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-05-18 09:24:47 -0700

    Add files via upload

---
+++ car.js
@@ -0,0 +1,21 @@
+function what() {
+    window.location = "what.html"
+}
+function home() {
+    window.location = "index.html"
+}
+function where() {
+    window.location = "where.html"
+}
+function symp() {
+    window.location = "symp.html"
+}
+function prev() {
+    window.location = "prev.html"
+}
+function treat() {
+    window.location = "treat.html"
+}
+function don() {
+    window.location = "donate.html"
+}
